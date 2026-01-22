ac65c745b116f263a8720705d9e0a1f72d3b49bb
gianlucacasullo10-creator <gianlucacasullo10@gmail.com>
2026-01-22 11:25:58 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { BottomNav } from "./components/BottomNav";
+import BottomNav from "./components/BottomNav";
 import Index from "./pages/Index";
 import Leaderboards from "./pages/Leaderboards";
 import Capture from "./pages/Capture";
